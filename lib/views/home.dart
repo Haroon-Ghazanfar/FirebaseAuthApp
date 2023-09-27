9daaa7ad259b8f31f2e651b5b8a4fbf96dd6bfb8
Haroon <haroonghazanfar321@gmail.com>
2023-09-27 15:42:36 +0500

    home screen animation added

--- lib/views/home.dart
+++ lib/views/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
 
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
@@ -21,7 +22,7 @@ class _HomeState extends State<Home> {
         backgroundColor: Colors.blue,
       ),
       body: Container(
-        child: const Column(
+        child: Column(
           children: [
             Center(
               child: Text(
@@ -30,6 +31,11 @@ class _HomeState extends State<Home> {
                   fontSize: 34,
                 ),
               ),
+            ),
+            Container(
+              height: 300,
+              width: MediaQuery.of(context).size.width,
+              child: Lottie.asset("assets/animation_ln1m1rf4.json"),
             ),
           ],
         ),
